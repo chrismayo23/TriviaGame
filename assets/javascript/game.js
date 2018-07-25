0f87bd5f401428497d8ab66122420471a696bb7a
gauguin <chrismayo23@gmail.com>
2018-07-25 01:35:13 -0400

    added javascript arrays

---
+++ assets/javascript/game.js
@@ -0,0 +1,42 @@
+// Make arrays for questions, possible answers, correct answers, and image file names
+var questions = [
+    "This NFL quarterback from the 1990's led his team to 4 consecutive Super Bowl appearances while innovating the &quot;no-huddle&quot; offense.",
+    "This bicycle racer, nicknamed &quot;The Cannibal&quot;, rocketed his way to 5 Tour de France wins between 1969 and 1974.",
+    "This track and field star won the 100 meter and 200 meter dash at the 1988 Olympics on her way to 3 gold medals.",
+    "This tennis legend won the 2017 Australian Open to surpass Steffi Graf's record of 22 major championships.",
+    "This New York Yankee had his 1952 Tops baseball card sell for $2.88 million at a 2018 sports memorabilia auction.",
+    "This basketball mega-star signed to play for the Los Angeles Lakers in 2018 after winning championships with Miami Heat and Cleveland Cavaliers.",
+    "This legendary golfer was the only one to complete the Grand Slam by winning all 4 major championships in a single year. He would go on to design Augusta National Golf Club and found The Masters tournament.",
+    "This multi-sport athlete was raised in the Sac and Fox Indian tribe. He won 2 gold medals in the 1912 Olympics and went on to star in NFL in the 1920's.",
+    "This pioneer in women's sport won 2 gold medals in the 1932 Olympics and went on to win 10 major championships as an LPGA Golfer.",
+    "This track and field athlete entered the 1936 Olympics in Berlin and took home a record 4 gold medals.",
+];
+
+var possibleAnswers = [
+    ["John Elway", "Tom Brady", "Jim Kelly", "Dan Marino"],
+    ["Lance Armstrong", "Eddy Merckx", "Miguel Indurain", "Chris Froome"],
+    ["Jackie Joyner-Kersey", "Allyson Felix", "Brenda Martinez", "Florence Griffith-Joyner")],
+    ["Venus Williams", "Chris Evert", "Serena Williams", "Monica Seles"],
+    ["Mickey Mantle", "Babe Ruth", "Lou Gehrig", "Joe DiMaggio"],
+    ["Michael Jordan", "Wilt Chamberlain", "Kobe Bryant", "LeBron James"],
+    ["Jack Nicklaus", "Bobby Jones", "Old Tom Morris", "Tiger Woods"],
+    ["Jim Thorpe", "Jim Brown", "Muhammad Ali", "Red Grange"],
+    ["Nancy Lopez", "Billy Jean King", "Babe Didrikson Zaharias", "Anika Sorenstam"],
+    ["Carl Lewis", "Usain Bolt", "Michael Phelps", "Jesse Owens"],
+];
+
+var answers = [2, 1, 3, 2, 0, 3, 1, 0, 2, 3];
+
+var imageName = [
+    "kelly.jpg",
+    "merckx.jpg",
+    "flojo.gif",
+    "serena.jpg",
+    "mickey.png",
+    "lebron.gif",
+    "jones.jpg",
+    "thorpe.jpg",
+    "babe.jpg",
+    "owens.gif",
+]
+
