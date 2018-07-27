b40900bf50289ebc8f41f0a564bc2850115a7c13
gauguin <chrismayo23@gmail.com>
2018-07-26 21:51:51 -0400

    changed some of the questions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,40 +1,40 @@
 // Make arrays for questions, possible answers, correct answers, and image file names
 var questions = [
+    "This tennis legend won the 2017 Australian Open to surpass Steffi Graf's record of 22 major championships.",
+    "This multi-sport athlete was raised in the Sac and Fox Indian tribe. He won 2 gold medals in the 1912 Olympics and went on to star in NFL in the 1920's.",
+    "This track and field athlete entered the 1936 Olympics in Berlin and took home a record 4 gold medals.",
     "This NFL quarterback from the 1990's led his team to 4 consecutive Super Bowl appearances while innovating the \"no-huddle\" offense.",
+    "This pioneer in women's sport won 2 gold medals in the 1932 Olympics and went on to win 10 major championships as an LPGA Golfer.",
     "This bicycle racer, nicknamed \"The Cannibal\", rocketed his way to 5 Tour de France wins between 1969 and 1974.",
     "This track and field star won the 100 meter and 200 meter dash at the 1988 Olympics on her way to 3 gold medals.",
-    "This tennis legend won the 2017 Australian Open to surpass Steffi Graf's record of 22 major championships.",
     "This New York Yankee had his 1952 Tops baseball card sell for $2.88 million at a 2018 sports memorabilia auction.",
     "This basketball mega-star signed to play for the Los Angeles Lakers in 2018 after winning championships with Miami Heat and Cleveland Cavaliers.",
     "This legendary golfer was the only one to complete the Grand Slam by winning all 4 major championships in a single year. He would go on to design Augusta National Golf Club and found The Masters tournament.",
-    "This multi-sport athlete was raised in the Sac and Fox Indian tribe. He won 2 gold medals in the 1912 Olympics and went on to star in NFL in the 1920's.",
-    "This pioneer in women's sport won 2 gold medals in the 1932 Olympics and went on to win 10 major championships as an LPGA Golfer.",
-    "This track and field athlete entered the 1936 Olympics in Berlin and took home a record 4 gold medals.",
 ];
 var possibleAnswers = [
+    ["Venus Williams", "Chris Evert", "Serena Williams", "Monica Seles"],
+    ["Jim Thorpe", "Jim Brown", "Muhammad Ali", "Red Grange"],
+    ["Carl Lewis", "Usain Bolt", "Michael Phelps", "Jesse Owens"],
     ["John Elway", "Tom Brady", "Jim Kelly", "Dan Marino"],
+    ["Nancy Lopez", "Billy Jean King", "Babe Didrikson Zaharias", "Anika Sorenstam"],
     ["Lance Armstrong", "Eddy Merckx", "Miguel Indurain", "Chris Froome"],
     ["Jackie Joyner-Kersey", "Allyson Felix", "Brenda Martinez", "Florence Griffith-Joyner"],
-    ["Venus Williams", "Chris Evert", "Serena Williams", "Monica Seles"],
     ["Mickey Mantle", "Babe Ruth", "Lou Gehrig", "Joe DiMaggio"],
     ["Michael Jordan", "Wilt Chamberlain", "Kobe Bryant", "LeBron James"],
     ["Jack Nicklaus", "Bobby Jones", "Old Tom Morris", "Tiger Woods"],
-    ["Jim Thorpe", "Jim Brown", "Muhammad Ali", "Red Grange"],
-    ["Nancy Lopez", "Billy Jean King", "Babe Didrikson Zaharias", "Anika Sorenstam"],
-    ["Carl Lewis", "Usain Bolt", "Michael Phelps", "Jesse Owens"],
 ];
-var answers = [2, 1, 3, 2, 0, 3, 1, 0, 2, 3];
+var answers = [2, 0, 3, 2, 2, 1, 3, 0, 3, 1];
 var imageName = [
+    "serena.jpg",
+    "thorpe.jpg",
+    "owens.jpg",
     "kelly.jpg",
+    "babe.jpg",
     "merckx.jpg",
     "flojo.jpg",
-    "serena.jpg",
     "mickey.jpg",
     "lebron.gif",
     "jones.jpg",
-    "thorpe.jpg",
-    "babe.jpg",
-    "owens.jpg",
 ];
 
 // Hide other tiles upon loading
